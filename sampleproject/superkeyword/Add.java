a40058f8a9029e8cf35c64451f07f5bbbe71b188
vishnubrahmaputran <vishnubrahmaputran@localhost>
2024-01-18 07:40:25 +0400

    commit sampleproject

---
+++ sampleproject/superkeyword/Add.java
@@ -0,0 +1,15 @@
+package superkeyword;
+
+public class Add {
+	public static void main(String[] args) {
+		
+	}
+
+public int adds(int a,int b)
+
+{
+	int sum=a+b;
+	return sum;
+}
+	
+}
---
+++ sampleproject/superkeyword/DivisibilityCheck.java
@@ -0,0 +1,22 @@
+package superkeyword;
+
+public class DivisibilityCheck extends Add{
+	public static void main(String[] args) {
+		DivisibilityCheck obj=new DivisibilityCheck();
+		obj.divisibility();	
+	}
+	
+	public void divisibility()
+	
+	{
+		int s=super.adds(10,20);
+		if(s%10==0)
+			System.out.println("The sum is divisible by 10");
+		else
+			System.out.println("The sum is not divisible by 10");		
+	}
+	
+}
+
+	
+	
